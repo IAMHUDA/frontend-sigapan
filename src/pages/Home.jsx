90b13eecd4a465ccfe5c80ae2e889ae51c1907d9
IAMHUDA <hudamiftahul362@gmail.com>
2025-07-19 22:17:11 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+  return (
+    <>
+    <div className='font-carousel text-3xl font-bold underline'>
+        <h1>halo dunia</h1>
+    </div>
+    </>
+  )
+}
+
+export default Home
